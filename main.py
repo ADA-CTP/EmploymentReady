dc702298c477dcd12d19cc8d77b147bf684fe6ed
DTang127 <93285387+DTang127@users.noreply.github.com>
2023-11-28 15:01:44 -0500

    added all user input options

--- main.py
+++ main.py
@@ -45,46 +45,65 @@ if app_mode == 'Home':
 elif app_mode == 'Prediction':   
     st.subheader('Please enter all necessary informations to calculate your employability as a Software Developer!')    
     st.sidebar.header("Input your information here:")    
-    #gender=0, age=0, accessibility=0, employment=0, mental_health=0, main_branch=0, years_code=0, years_code_pro=0, previous_salary=0, education='', tools=
+    
     gender_dict = {"Male":0,"Female":1,"Non-Binary":2} 
     gender=st.sidebar.radio('Gender',tuple(gender_dict.keys())) 
-    st.markdown('Gender: '+str(gender))
+    st.markdown('''Gender: **:blue[{}]**'''.format(gender))
 
     age_dict = {"Older than 35":0,"Younger than 35":1} 
     age=st.sidebar.radio('Age',tuple(age_dict.keys())) 
-    st.markdown('Age: '+str(age))
+    st.markdown('Age: **:blue[{}]**'''.format(age))
 
     accessibility_dict = {"No":0,"Yes":1} 
-    accessibility=st.sidebar.radio('Accessibility',tuple(age_dict.keys())) 
-    st.markdown('Accessibility: '+str(accessibility))
+    accessibility=st.sidebar.radio('Any disabilities?',tuple(accessibility_dict.keys())) 
+    st.markdown('Accessibility: **:blue[{}]**'''.format(accessibility))
 
-    # feature_dict = {"No":1,"Yes":2}    
-    # edu={'Graduate':1,'Not Graduate':2}    
-    # prop={'Rural':1,'Urban':2,'Semiurban':3}    
-    # ApplicantIncome=st.sidebar.slider('ApplicantIncome',0,10000,0,)    
-    # CoapplicantIncome=st.sidebar.slider('CoapplicantIncome',0,10000,0,)    
-    # LoanAmount=st.sidebar.slider('LoanAmount in K$',9.0,700.0,200.0)    
-    # Loan_Amount_Term=st.sidebar.selectbox('Loan_Amount_Term',(12.0,36.0,60.0,84.0,120.0,180.0,240.0,300.0,360.0))    
-    # Credit_History=st.sidebar.radio('Credit_History',(0.0,1.0))    
-       
-    # Married=st.sidebar.radio('Married',tuple(feature_dict.keys()))    
-    # Self_Employed=st.sidebar.radio('Self Employed',tuple(feature_dict.keys()))    
-    # Dependents=st.sidebar.radio('Dependents',options=['0','1' , '2' , '3+'])    
-    # Education=st.sidebar.radio('Education',tuple(edu.keys()))    
-    # Property_Area=st.sidebar.radio('Property_Area',tuple(prop.keys()))    
-    # class_0, class_3, class_1, class_2 = 0,0,0,0    
-    # if Dependents == '0':        
-    #     class_0 = 1    
-    # elif Dependents == '1':        
-    #     class_1 = 1    
-    # elif Dependents == '2' :        
-    #     class_2 = 1    
-    # else:        
-    #     class_3= 1    
-    # Rural,Urban,Semiurban=0,0,0    
-    # if Property_Area == 'Urban' :        
-    #     Urban = 1    
-    # elif Property_Area == 'Semiurban' :        
-    #     Semiurban = 1    
-    # else :        
-    #     Rural = 1
+    employment_dict = {"No":0,"Yes":1} 
+    employment=st.sidebar.radio('Current Employed?',tuple(employment_dict.keys())) 
+    st.markdown('Employment Status: **:blue[{}]**'''.format(employment))
+
+    mental_health_dict = {"No":0,"Yes":1} 
+    mental_health=st.sidebar.radio('Mental Health Issues?',tuple(mental_health_dict.keys())) 
+    st.markdown('Mental Health: **:blue[{}]**'''.format(mental_health))
+
+    main_branch_dict = {"Non-Developer":0,"Developer":1} 
+    main_branch=st.sidebar.radio('Are you a Developer?',tuple(main_branch_dict.keys())) 
+    st.markdown('Developer: **:blue[{}]**'''.format(main_branch))
+
+    years_code=st.sidebar. slider('Years of Coding?',0,50,0,)  
+    st.markdown('Years of Coding: **:blue[{}]**'''.format(years_code))
+
+    years_code_pro=st.sidebar.slider('Years of Coding Professionally?',0,50,0,)  
+    st.markdown('Years of Coding Professionally: **:blue[{}]**'''.format(years_code_pro))
+
+    prev_salary=st.sidebar.number_input('Most Recent Annual Salary?',value=0,step=1)  
+    st.markdown('Most Recent Annual Salary: **:blue[{}]**'''.format(prev_salary))
+
+    st.sidebar.markdown('Education Level?') 
+    edu_lvl_nohighered=st.sidebar.checkbox('No Higher Education') 
+    edu_lvl_undergrad=st.sidebar.checkbox('Undergraduate') 
+    edu_lvl_master=st.sidebar.checkbox('Master') 
+    edu_lvl_phd=st.sidebar.checkbox('PHD') 
+    edu_lvl_other=st.sidebar.checkbox('Other')
+    education = []
+    if edu_lvl_nohighered:
+        education.append('No Higher Education')
+    if edu_lvl_undergrad:
+        education.append('Undergraduate')  
+    if edu_lvl_master:
+        education.append('Master')  
+    if edu_lvl_phd:
+        education.append('PHD')  
+    if edu_lvl_other:
+        education.append('Other')     
+    education = ', '.join(education) 
+    st.write('Education Level: **:blue[{}]**'''.format(education))
+
+    tech=st.sidebar.text_area('Technologies (please separate with commas thank you):')
+    st.markdown('Technologies: **:blue[{}]**'''.format(tech))
+
+    code_language=st.sidebar.text_area('Coding Languages (please separate with commas thank you):')
+    st.markdown('Coding Languages: **:blue[{}]**'''.format(code_language))
+
+    st.subheader('Please confirm the information above before using our prediction model.')
+     #if st.button("Predict"):        file_ = open("6m-rain.gif", "rb")        contents = file_.read()        data_url = base64.b64encode(contents).decode("utf-8")        file_.close()        file = open("green-cola-no.gif", "rb")        contents = file.read()        data_url_no = base64.b64encode(contents).decode("utf-8")        file.close()        loaded_model = pickle.load(open('Random_Forest.sav', 'rb'))        prediction = loaded_model.predict(single_sample)        if prediction[0] == 0 :            st.error(    'According to our Calculations, you will not get the loan from Bank'    )            st.markdown(    f'<img src="data:image/gif;base64,{data_url_no}" alt="cat gif">',    unsafe_allow_html=True,)        elif prediction[0] == 1 :            st.success(    'Congratulations!! you will get the loan from Bank'    )            st.markdown(    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',    unsafe_allow_html=True,    )
